a1ef2df00c0cd3a1559dea80a28cb6b2888d9164
aaa <hoonrara2@gmail.com>
2025-07-08 01:44:37 +0900

    refactor: N+1 문제 해결

--- src/main/java/com/example/payday/user/repository/UserProfileRepository.java
+++ src/main/java/com/example/payday/user/repository/UserProfileRepository.java
@@ -1,14 +1,17 @@
 package com.example.payday.user.repository;
 
 import com.example.payday.user.domain.UserProfile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long>, UserProfileCustomRepository {
 
-    Optional<UserProfile> findByUser_Id(Long userId);
-    Optional<UserProfile> findByUserId(Long userId);
+    Optional<UserProfile> findWithUserByUserId(Long userId);
+
 
 
 }
